ee17ac38fcef2622ea2dbc8002ce2c608f7a1c59
Gabriel <gabrielmonteiroduete@gmail.com>
2025-03-13 19:23:36 -0300

    chore: configure theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
+import { LocalKeys } from '@/enums/LocalKeys'
 import GlobalStyle from '@/styles/GlobalStyles'
+import { darkTheme, lightTheme } from '@/styles/theme'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const theme =
+    (typeof window !== 'undefined' &&
+      window.localStorage.getItem(LocalKeys.THEME)) ||
+    'dark'
+
   return (
-    <>
+    <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
