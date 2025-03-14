131b3e8924850aa449042ea2021f46cd028f47ef
Gabriel <gabrielmonteiroduete@gmail.com>
2025-03-13 22:01:15 -0300

    feat: add style is select when path is selected

--- src/layout/components/header/components/navigator/index.tsx
+++ src/layout/components/header/components/navigator/index.tsx
@@ -1,13 +1,25 @@
+import { useRouter } from 'next/router'
+
 import items from './navigator.data'
 import * as S from './styles'
 
 const Navigator = () => {
+  const router = useRouter()
+
+  const isSelect = (path: string) => {
+    const query = router.pathname
+
+    return (
+      query === `/${path.toLowerCase()}` || (query === '/' && path === 'Hello')
+    )
+  }
+
   return (
     <S.Items>
       {items.map(({ name, href }) => {
         return (
           <S.Item key='name'>
-            <S.Link href={href} target='_self' isSelect={false}>
+            <S.Link href={href} target='_self' isSelect={isSelect(name)}>
               {name}
             </S.Link>
           </S.Item>
