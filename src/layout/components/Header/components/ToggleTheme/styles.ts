fef709250c2eb5f71a82391c2acb358648484d1e
Gabriel <gabrielmonteiroduete@gmail.com>
2025-04-03 00:09:42 -0300

    refactor: use dinamic dark theme

--- src/layout/components/Header/components/ToggleTheme/styles.ts
+++ src/layout/components/Header/components/ToggleTheme/styles.ts
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
 
-export const Circle = styled.div`
-  width: 24px;
-  height: 24px;
-  background-color: var(--color-green-neutral);
+export const Circle = styled.div<{ isDark: boolean }>`
+  width: var(--spacing-large);
+  height: var(--spacing-large);
+  background-color: ${({ isDark }) =>
+    isDark ? 'var(--color-green-neutral)' : 'var(--color-green-border)'};
   border-radius: 50%;
   cursor: pointer;
 `
