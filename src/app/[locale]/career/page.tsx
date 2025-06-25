7b2a8841716428c2735d3a0ffe3c0c431a73638b
Gabriel <gabrielmonteiroduete@gmail.com>
2025-06-24 21:44:25 -0300

    feat: add experiences data and update career page with experience selection

--- src/app/[locale]/career/page.tsx
+++ src/app/[locale]/career/page.tsx
@@ -1,28 +1,65 @@
+'use client'
+
+import { useState } from 'react'
+
+import clsx from 'clsx'
 import { useTranslations } from 'next-intl'
 
+import { experiences } from './data'
+import { IExperiences } from './types'
+
 const Career = () => {
+  const [selectedExperience, setSelectedExperience] =
+    useState<IExperiences>('Juntos Somos Mais')
+
   const t = useTranslations('CarrerPage')
 
-  const contributionKeys = [
-    'contrib1',
-    'contrib2',
-    'contrib3',
-    'contrib4',
-    'contrib5',
-    'contrib6',
-  ] as const
+  const experience = `Experiences.${selectedExperience}`
 
-  console.debug(t('CarrerPage'))
+  const totalContributions = Number(t(`${experience}.totalContributions`))
+
+  const contributionKeys = Array.from({ length: totalContributions }, (_, i) =>
+    (i + 1).toString(),
+  )
 
   return (
-    <div>
-      <h1>{t('JSM.title')}</h1>
-      <ul>
-        {contributionKeys.map(key => (
-          <li key={key}>{t(`JSM.contributions.${key}`)}</li>
+    <section className='flex gap-giant'>
+      <div
+        className='
+            w-[312px] p-xxlarge bg-bg-primary rounded-sm text-white flex-shrink-0 self-start
+            border-[1px] border-green-weak-border flex flex-col gap-small
+        '
+      >
+        {experiences.map(experience => (
+          <button
+            key={experience}
+            onClick={() => setSelectedExperience(experience as IExperiences)}
+            className={clsx(
+              'rounded-sm bg-green-weak-border w-full w-max-[248px] p-small cursor-pointer text-large hover:bg-secondary hover:text-black transition-colors duration-500',
+              selectedExperience === experience &&
+                'bg-secondary text-black cursor-default',
+            )}
+          >
+            {experience}
+          </button>
         ))}
-      </ul>
-    </div>
+      </div>
+      <div>
+        <h1 className='text-green-neutral text-title-giant'>
+          {t(`${experience}.title`)}
+        </h1>
+        <p className='text-green-white text-medium mb-xsmall'>
+          {t(`${experience}.time`)}
+        </p>
+        <ul className='flex flex-col gap-small'>
+          {contributionKeys.map(contribution => (
+            <li key={contribution} className='text-large text-primary'>
+              • {t(`${experience}.contributions.${contribution}`)}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   )
 }
 
