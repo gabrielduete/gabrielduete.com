b7e361198c31b396a5363669e7a7893ced640596
Gabriel <gabrielmonteiroduete@gmail.com>
2025-06-27 20:49:51 -0300

    feat: implement filter functionality in Blog component with default selection

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,11 +1,40 @@
-import { getTranslations } from 'next-intl/server'
+'use client'
 
-const Blog = async () => {
-  const t = await getTranslations('IndexPage')
+import { useEffect } from 'react'
+
+import Filter from '@/components/Filter'
+import { IFilters } from '@/components/Filter/types'
+import { useRouter, useSearchParams } from 'next/navigation'
+
+const Blog = () => {
+  const searchParams = useSearchParams()
+  const router = useRouter()
+
+  const DEFAULT_FILTER: IFilters = 'Front-end'
+
+  const selectedFilter =
+    (searchParams.get('filter') as IFilters) || DEFAULT_FILTER
+
+  useEffect(() => {
+    const hasFilterParam = searchParams.has('filter')
+
+    if (!hasFilterParam) {
+      const params = new URLSearchParams(searchParams.toString())
+
+      params.set('filter', DEFAULT_FILTER)
+      router.replace(`?${params.toString()}`)
+    }
+  }, [searchParams, router])
+
+  const filterChange = (filter: string) => {
+    const params = new URLSearchParams(searchParams.toString())
+    params.set('filter', filter)
+    router.push(`?${params.toString()}`)
+  }
 
   return (
     <div>
-      <h1>{t('title')}</h1>
+      <Filter selectedFilter={selectedFilter} onFilterChange={filterChange} />
     </div>
   )
 }
