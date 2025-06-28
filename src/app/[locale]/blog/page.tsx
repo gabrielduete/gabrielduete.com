371b3265fc02963e80e5b0635bb96dfa20609834
Gabriel <gabrielmonteiroduete@gmail.com>
2025-06-28 01:07:24 -0300

    feat: integrate Cards component into Blog page for article display

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -3,12 +3,15 @@
 import Filter from '@/components/Filter'
 import { useFilter } from '@/hooks/useFilter'
 
+import Cards from './components/Cards'
+
 const Blog = () => {
   const { selectedFilter, setFilter } = useFilter()
 
   return (
-    <div>
+    <div className='flex flex-col gap-xxlarge'>
       <Filter selectedFilter={selectedFilter} onFilterChange={setFilter} />
+      <Cards />
     </div>
   )
 }
