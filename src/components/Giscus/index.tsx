b15a39f954c253d27ab4bf9ca40045f8106e713b
Gabriel <gabrielmonteiroduete@gmail.com>
2025-07-05 01:33:01 -0300

    fix: remove loading attribute from GiscusComments component for cleaner rendering

--- src/components/Giscus/index.tsx
+++ src/components/Giscus/index.tsx
@@ -25,7 +25,6 @@ export const GiscusComments = ({
         inputPosition='bottom'
         theme='dark_dimmed'
         lang={locale === Locales.EN ? 'en' : 'pt'}
-        loading='lazy'
       />
     </div>
   )
