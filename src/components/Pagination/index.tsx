a83e52e04bfe087f9fec4f0691149723ff74ffce
Gabriel <gabrielmonteiroduete@gmail.com>
2025-07-12 10:39:30 -0300

    refactor: simplify Pagination component structure by removing unnecessary div wrapper

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -12,24 +12,22 @@ const Pagination = ({
   onPageChange,
 }: PaginationProps) => {
   return (
-    <div>
-      <nav className='flex justify-center items-center gap-medium'>
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button
-            key={index}
-            onClick={() => onPageChange(index + 1)}
-            className={clsx(
-              'bg-bg-primary rounded-md cursor-pointer p-xsmall pb-xxsmall',
-              currentPage === index + 1
-                ? 'text-secondary'
-                : 'text-primary hover:text-secondary',
-            )}
-          >
-            {index + 1}
-          </button>
-        ))}
-      </nav>
-    </div>
+    <nav className='flex justify-center items-center gap-medium'>
+      {Array.from({ length: totalPages }, (_, index) => (
+        <button
+          key={index}
+          onClick={() => onPageChange(index + 1)}
+          className={clsx(
+            'bg-bg-primary rounded-md cursor-pointer p-xsmall pb-xxsmall',
+            currentPage === index + 1
+              ? 'text-secondary'
+              : 'text-primary hover:text-secondary',
+          )}
+        >
+          {index + 1}
+        </button>
+      ))}
+    </nav>
   )
 }
 
