761550eb6bf53e21743d0213692709ac2eb5aca1
Gabriel <gabrielmonteiroduete@gmail.com>
2025-07-27 20:03:24 -0300

    refactor: remove locale validation check from layout component

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -26,10 +26,6 @@ type Props = {
 const LocaleLayout = async ({ children, params }: Props) => {
   const { locale } = await params
 
-  if (!hasLocale(routing.locales, locale)) {
-    notFound()
-  }
-
   return (
     <html lang={locale}>
       <body>
