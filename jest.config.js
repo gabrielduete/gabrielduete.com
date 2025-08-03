1dc49accd567b4d03640746d7d8f5acfe31b7f2e
Gabriel <gabrielmonteiroduete@gmail.com>
2025-08-03 00:40:08 -0300

    chore: configure codecov

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,15 @@ const customJestConfig = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/_*.{js,ts,tsx}',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
+  testEnvironment: 'jsdom',
 }
 
 module.exports = createJestConfig(customJestConfig)
