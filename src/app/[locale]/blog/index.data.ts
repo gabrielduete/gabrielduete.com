d61c94a404432c8c57a8145ccc46a60794249ecb
Gabriel <gabrielmonteiroduete@gmail.com>
2025-10-19 00:02:09 -0300

    fix: update Portuguese blog filters for consistency
    - Changed the category name from 'Engineering' to 'Engenharia' in the Portuguese filters to ensure accurate localization.

--- src/app/[locale]/blog/index.data.ts
+++ src/app/[locale]/blog/index.data.ts
@@ -1,4 +1,4 @@
 export const FILTERS = {
-  'pt-br': ['Todos', 'Engineering', 'Front-end', 'Úteis'],
+  'pt-br': ['Todos', 'Engenharia', 'Front-end', 'Úteis'],
   en: ['All', 'Engineering', 'Front-end', 'Utils'],
 } as const
